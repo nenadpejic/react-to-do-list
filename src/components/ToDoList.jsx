0a3a3e0e02b3e454460c3bf3bff8c2f50a35e1cd
nenadpejic <npejic89@gmail.com>
2020-10-05 17:31:18 +0200

    Added svgs for add and del buttons and styled them

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -32,7 +32,6 @@ const Input = styled.input`
   border-radius: 0.25rem;
   padding: 0.5rem 1rem;
   padding-right: 3rem;
-  position: relative;
   width: 100%;
 
   &:focus {
@@ -48,8 +47,18 @@ const Button = styled.button`
   background: transparent;
   border: none;
   width: 3rem;
-  color: ${(props) => props.theme.mainDark};
   font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > svg {
+    width: 1.5rem;
+    fill: ${(props) => props.theme.mainDark};
+  }
+  &:hover > svg {
+    fill: #fff;
+  }
 
   &:hover {
     cursor: pointer;
@@ -93,7 +102,14 @@ class ToDoList extends Component {
               placeholder="Add item"
               autoComplete="off"
             />
-            <Button type="submit">Add</Button>
+            <Button type="submit">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <g>
+                  <path d="M256,0C114.833,0,0,114.833,0,256s114.833,256,256,256s256-114.853,256-256S397.167,0,256,0z M256,472.341 c-119.275,0-216.341-97.046-216.341-216.341S136.725,39.659,256,39.659S472.341,136.705,472.341,256S375.295,472.341,256,472.341z" />
+                  <path d="M355.148,234.386H275.83v-79.318c0-10.946-8.864-19.83-19.83-19.83s-19.83,8.884-19.83,19.83v79.318h-79.318 c-10.966,0-19.83,8.884-19.83,19.83s8.864,19.83,19.83,19.83h79.318v79.318c0,10.946,8.864,19.83,19.83,19.83 s19.83-8.884,19.83-19.83v-79.318h79.318c10.966,0,19.83-8.884,19.83-19.83S366.114,234.386,355.148,234.386z" />
+                </g>
+              </svg>
+            </Button>
           </Div>
         </form>
         <Div window>
